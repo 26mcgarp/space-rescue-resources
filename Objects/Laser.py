32fb76f4c29b467b1ce2f8557ce92eccc2ff0be6
26mcgarp <26mcgarp@mbbc.qld.edu.au>
2024-03-01 15:07:37 +1000

    Added Sounds

--- Objects/Laser.py
+++ Objects/Laser.py
@@ -42,9 +42,11 @@ class Laser(RoomObject):
         Handles laser collisions with other registered objects
         """
         if other_type == "Asteroid":
+            self.room.asteroid_shot.play()
             self.room.delete_object(other)
             self.room.score.update_score(5)
             #print("Asteroid Deleted")
         elif other_type == "Astronaut":
+            self.room.astronaut_shot.play()
             self.room.delete_object(other)
             self.room.score.update_score(-10)
